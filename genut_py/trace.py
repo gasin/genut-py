44499e150156dd407c37492c4419ef3f41266045
gasin <7404299+gasin@users.noreply.github.com>
2023-11-01 22:36:46 +0900

    trace active mode only

--- genut_py/trace.py
+++ genut_py/trace.py
@@ -23,6 +23,8 @@ class FuncTrace:
 class Tracer:
     _index = 0
     func_traces: dict[int, FuncTrace] = {}
+    is_opcode_trace = False
+    is_line_trace = False
 
     @classmethod
     def register(cls, filename, line_start, line_end, is_line) -> int:
@@ -31,6 +33,10 @@ class Tracer:
 
         cls._index += 1
         cls.func_traces[cls._index] = FuncTrace(filename, line_start, line_end, is_line)
+        if is_line:
+            cls.is_line_trace = True
+        else:
+            cls.is_opcode_trace = True
         return cls._index
 
     @classmethod
@@ -44,10 +50,13 @@ class Tracer:
         if len(cls.func_traces) == 0:
             sys.settrace(None)
 
+        cls.is_line_trace = any(ft.is_line for ft in cls.func_traces.values())
+        cls.is_opcode_trace = any(not ft.is_line for ft in cls.func_traces.values())
+
     @classmethod
     def trace(cls, frame, event, arg):
-        frame.f_trace_lines = True
-        frame.f_trace_opcodes = True
+        frame.f_trace_lines = cls.is_line_trace
+        frame.f_trace_opcodes = cls.is_opcode_trace
         if event == "line":
             for func_trace in cls.func_traces.values():
                 if func_trace.is_line:
